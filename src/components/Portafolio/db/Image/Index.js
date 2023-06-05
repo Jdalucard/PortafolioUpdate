e67a214521238c4441ba11a236a28af939e1ae48
Jose Daniel <99700736+Jdalucard@users.noreply.github.com>
2023-06-05 13:30:33 -0500

    "data9"

--- src/components/Portafolio/db/Image/Index.js
+++ src/components/Portafolio/db/Image/Index.js
@@ -6,4 +6,6 @@ import IMG5 from "../../../../assets/Crud.jpeg";
 import IMG6 from "../../../../assets/Sociapedia.jpeg";
 import IMG7 from "../../../../assets/filterstate.jpeg";
 import IMG8 from "../../../../assets/todoapp.jpeg";
-export { IMG1, IMG2, IMG3, IMG4, IMG5, IMG6, IMG7, IMG8 };
+import IMG9 from "../../../../assets/ip.jpeg";
+
+export { IMG1, IMG2, IMG3, IMG4, IMG5, IMG6, IMG7, IMG8, IMG9 };
